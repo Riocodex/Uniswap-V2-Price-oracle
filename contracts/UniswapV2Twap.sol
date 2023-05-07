599e0d5860787e6e8e195324b9e0330a29bb736d
Rio <onwukachibike@gmail.com>
2023-05-07 22:25:47 +0100

    updated

---
+++ contracts/UniswapV2Twap.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.6.6;
+// NOTE: using solidity 0.6.6 to match imports
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/lib/contracts/libraries/FixedPoint.sol";
+import "@uniswap/v2-periphery/contracts/libraries/UniswapV2OracleLibrary.sol";
+import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
