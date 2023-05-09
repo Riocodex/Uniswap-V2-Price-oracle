2dec88a260556abd6217f5c07dc8f6bd0680b750
Rio <onwukachibike@gmail.com>
2023-05-09 02:34:31 +0100

    created contract

--- contracts/UniswapV2Twap.sol
+++ contracts/UniswapV2Twap.sol
@@ -5,3 +5,20 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/lib/contracts/libraries/FixedPoint.sol";
 import "@uniswap/v2-periphery/contracts/libraries/UniswapV2OracleLibrary.sol";
 import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
+
+contract UniswapV2Twap {
+    //in uniswapv2 the number are represented in decimals and solidity doesnt have them so they created their own using fixed point, to use this we declare fixed point
+    using FixedPoint for *;
+
+    //this will be the time to wait before we updated twap
+    uint public constant PERIOD = 10;
+
+    IUniswapV2Pair public immutable pair;
+    address public immutable token0;
+    address public immutable token1;
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+
+
+}
